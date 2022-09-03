53d013b5f5f77cfed4a5a5917a4f16d17545f146
Lukáš M <luki.m2k@gmail.com>
2022-09-03 16:12:19 +0200

    updated `!csla` command (embedded)

--- cslabot.js
+++ cslabot.js
@@ -44,7 +44,7 @@ client.on('message',message => {
   if (message.content.startsWith(prefix + 'help')) {
     let helpEmbed = new Discord.MessageEmbed()
     .setColor("40E0D0")
-    .setTitle("Command list | CSLA bot")
+    .setTitle("Command list | Cbot")
     .setThumbnail('http://kai.rf.gd/images/images.png')
     .setDescription("Here is a list of all available commands.")
     .addFields(
@@ -115,7 +115,24 @@ client.on('message',message => {
 }
 
 // info commands
-  if (message.content.startsWith(prefix + 'csla')) {message.channel.send("Get our CDLC and it's depencies from:\n\nCSLA: Iron Curtain (7.5 GB):\n<https://store.steampowered.com/app/1294440/Arma_3_Creator_DLC_CSLA_Iron_Curtain/>\n\nCSLA - Iron Curtain - Campaign (322.754 MB):\n<https://steamcommunity.com/sharedfiles/filedetails/?id=2534376765>\n\nCSLA - Iron Curtain - Operations (421.121 MB):\n<https://steamcommunity.com/sharedfiles/filedetails/?id=2719101174>\n\nCSLA - Iron Curtain - Desert Pack (139.663 MB):\n<https://steamcommunity.com/sharedfiles/filedetails/?id=2687332656>");}
+  if (message.content.startsWith(prefix + 'csla')) {
+    let cslaEmbed = new Discord.MessageEmbed()
+    .setColor("40E0D0")
+    .setTitle("CSLA: IC and packs | Cbot")
+    .setThumbnail('https://scontent.fbts6-1.fna.fbcdn.net/v/t1.18169-9/14322438_1303838882960602_6466372646523026116_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=UFXEd-MRUd8AX8y5wCf&_nc_ht=scontent.fbts6-1.fna&oh=00_AT_HMx0gylrW7oGlii2bLAD3t346ghW_hxjXnzvSTnYtjw&oe=6337D6E9')
+    .setDescription("Here is a list of all our products.")
+    .addFields(
+      { name: 'Arma 3 Creator DLC: CSLA Iron Curtain', value: 'Size: 7.5 GB\nPrice: 12,99€\nLink: [Steam Store](https://store.steampowered.com/app/1294440/Arma_3_Creator_DLC_CSLA_Iron_Curtain/)' },
+      { name: 'CSLA - Iron Curtain - Compatibility Data for Non-Owners', value: 'Size: 3.5 GB\nLink: [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=2503886780)' },
+      { name: 'CSLA - Iron Curtain - Desert Pack', value: 'Size: 139.663 MB\nLink: [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=2687332656)' },
+      { name: 'CSLA - Iron Curtain - Campaign', value: 'Size: 322.754 MB\nLink: [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=2534376765)' },
+      { name: 'CSLA - Iron Curtain - Operations', value: 'Size: 421.121 MB\nLink: [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=2719101174)' },
+      { name: 'CSLA - Iron Curtain - Desert Pack', value: 'Size: 139.663 MB\nLink: [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=2687332656)' },
+    )
+    .setFooter(`Feel free to mention me if you need me or use !help.`)
+    .setTimestamp()
+    message.channel.send(cslaEmbed)
+  }
   if (message.content.startsWith(prefix + 'biki')) {message.reply('our biki page is at https://community.bistudio.com/wiki/Category:CSLA:_Iron_Curtain');}
   if (message.content.startsWith(prefix + 'ft')) {message.reply('our Feedback Tracker project is at https://feedback.bistudio.com/project/view/59');}
 
